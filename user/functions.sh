c9e3d9b2560da8c946a11bd4c93ef45e7152570d
Wes Harrell <wes.harrell@reannz.co.nz>
2026-07-22 09:34:11 +1200

    created pure (standalone) branch.  Modified user config to match pathing

--- user/functions.sh
+++ user/functions.sh
@@ -2,12 +2,12 @@
 #Spack does not have a way to list all available environments so...here is a function to do that
 spack-envlist() {
     echo "=== Site environments in /opt/nesi/spack/zen3/spack-env ==="
-    ls /opt/nesi/spack/zen3/spack-env/ 2>/dev/null || echo "none"
+    ls /opt/nesi/spack/pure/zen3/spack-env/ 2>/dev/null || echo "none"
     echo ""
     echo "=== Below is a list of any environments you created ==="
     spack env list
     echo ""
     echo "To track a site environment run:"
-    echo "spack env track /opt/nesi/spack/zen3/spack-env/ENVNAME"
+    echo "spack env track /opt/nesi/spack/pure/zen3/spack-env/ENVNAME"
     echo ""
 }
--- user/init.sh
+++ user/init.sh
@@ -3,9 +3,9 @@
 # maybe should create and test for a hidden lockfile...
 if [ ! -f "${HOME}/.spack/config.yaml" ]; then
     mkdir -p "${HOME}/.spack"
-    cp /opt/nesi/spack/etc/spack-conf/user/config.yaml "${HOME}/.spack/config.yaml"
-    cp /opt/nesi/spack/etc/spack-conf/user/upstreams.yaml "${HOME}/.spack/upstreams.yaml"
-    cp /opt/nesi/spack/etc/spack-conf/user/modules.yaml "${HOME}/.spack/modules.yaml"
+    cp /opt/nesi/spack/pure/etc/spack-conf/user/config.yaml "${HOME}/.spack/config.yaml"
+    cp /opt/nesi/spack/pure/etc/spack-conf/user/upstreams.yaml "${HOME}/.spack/upstreams.yaml"
+    cp /opt/nesi/spack/pure/etc/spack-conf/user/modules.yaml "${HOME}/.spack/modules.yaml"
     echo ""
     echo "[Spack] Created the ~/.spack directory in your home"
     echo "[Spack] Copied config.yaml and other yaml files from templates to your ~/.spack dir"
